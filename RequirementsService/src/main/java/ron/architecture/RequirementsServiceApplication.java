9b10b420b26fbdee4159851a1123a02348402e68
Ron <cg12192001@gmail.com>
2020-06-15 20:12:47 +0800

    Init RequirementsService

---
+++ RequirementsService/src/main/java/ron/architecture/RequirementsServiceApplication.java
@@ -0,0 +1,13 @@
+package ron.architecture;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class RequirementsServiceApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(RequirementsServiceApplication.class, args);
+	}
+
+}
---
+++ RequirementsService/src/main/java/ron/architecture/controller/RequirementsController.java
@@ -0,0 +1,16 @@
+/**
+ * 
+ */
+package ron.architecture.controller;
+
+import org.springframework.web.bind.annotation.RestController;
+
+/**
+ * @author Ron.Tseng
+ *
+ * @date 2020-06-15 04:45:03
+ */
+@RestController
+public class RequirementsController {
+
+}
